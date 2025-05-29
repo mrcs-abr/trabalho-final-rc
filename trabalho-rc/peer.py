1b8fea19734751af4615ecb5bb46b40b68d98a18
Marcos Andre <marcos.br258@gmail.com>
2025-05-29 16:15:23 -0300

    Added RSA encription

--- trabalho-rc/peer.py
+++ trabalho-rc/peer.py
@@ -1,17 +1,25 @@
-import socket, json, hashlib
-from encrypt_utils import hash_password
+import socket, json
+from encrypt_utils import generate_rsa_keys, serialize_public_key, deserialize_public_key, encrypt_with_public_key, decrypt_with_private_key, hash_password
 
 class Peer:
-    def __init__(self, tracker_host="localhost", tracker_port = 6000, peer_port = 5500):
+    def __init__(self, tracker_host="localhost", tracker_port=6000, peer_port=5500):
         self.tracker_host = tracker_host
         self.tracker_port = tracker_port
         self.tracker_info = (tracker_host, tracker_port)
         self.peer_port = peer_port
         self.peer_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
+        
+        self.private_key, self.public_key = generate_rsa_keys()
+        self.public_key_str = serialize_public_key(self.public_key)
+        
         self.peer_socket.connect(self.tracker_info)
-    def start(self):
+        
+        # Key exchange with tracker
+        self.tracker_public_key = deserialize_public_key(
+            json.loads(self.peer_socket.recv(4096).decode())["public_key"])
+        self.peer_socket.send(json.dumps({"public_key": self.public_key_str}).encode())
 
+    def start(self):
         while True:
             print("========== Tela inicial Chatp2p ==========")
             print("Escolha uma opção: ")
@@ -46,15 +54,18 @@ class Peer:
             "port": self.peer_port,
         }
 
-        self.peer_socket.send(json.dumps(requisition).encode())
-        data = self.peer_socket.recv(1024).decode()
+        encrypted_requisition = encrypt_with_public_key(
+            self.tracker_public_key, json.dumps(requisition))
+        self.peer_socket.send(encrypted_requisition.encode())
+        
+        encrypted_data = self.peer_socket.recv(1024).decode()
+        data = decrypt_with_private_key(self.private_key, encrypted_data)
         response = json.loads(data)
         print(response)
 
         if response.get("status") == "ok":
             print(response.get("message"))
             self.process_chat_functions()
-
         else:
             print(response.get("message"))
             option = input("Deseja tentar novamente? [s/n]").strip()
@@ -77,15 +88,18 @@ class Peer:
                 "port": self.peer_port,
             }
 
-            self.peer_socket.send(json.dumps(requisition).encode())
-            data = self.peer_socket.recv(1024).decode()
+            encrypted_requisition = encrypt_with_public_key(
+                self.tracker_public_key, json.dumps(requisition))
+            self.peer_socket.send(encrypted_requisition.encode())
+            
+            encrypted_data = self.peer_socket.recv(1024).decode()
+            data = decrypt_with_private_key(self.private_key, encrypted_data)
             response = json.loads(data)
             print(response)
         else:
             print("Usuario ou senha invalidos")
             self.process_register()
 
-    
     def process_chat_functions(self):
         while True:
             print("========== Chatp2p ==========")
@@ -112,11 +126,6 @@ class Peer:
                 case 4:
                     ...
 
-
-
-
 if __name__ == "__main__":
     peer = Peer()
     peer.start()
-
-
--- trabalho-rc/tracker.py
+++ trabalho-rc/tracker.py
@@ -1,8 +1,10 @@
+# tracker.py
 import socket, json
 from threading import Thread
+from encrypt_utils import generate_rsa_keys, serialize_public_key, deserialize_public_key, encrypt_with_public_key, decrypt_with_private_key, hash_password
 
 class Tracker:
-    def __init__(self, host='0.0.0.0', port=6000, max_connec = 5):
+    def __init__(self, host='0.0.0.0', port=6000, max_connec=5):
         self.host = host
         self.port = port
         self.server_info = (host, port)
@@ -10,10 +12,12 @@ class Tracker:
         self.server.bind(self.server_info)
         self.server.listen(max_connec)
 
-        # Chat storage
+        self.private_key, self.public_key = generate_rsa_keys()
+        self.public_key_str = serialize_public_key(self.public_key)
+
         self.users = {
             "tonhao": {
-                "password": "senha123",
+                "password": hash_password("senha123"),  
                 'active': False
             }
         }
@@ -29,23 +33,31 @@ class Tracker:
             print("Conexao de: " + str(address))
             Thread(target=self.process_new_peer, args=(peer_conec, address)).start()
 
-    def process_new_peer(self, peer_conec, address):  
+    def process_new_peer(self, peer_conec, address):
+        peer_conec.send(json.dumps({"public_key": self.public_key_str}).encode())
+        
+        peer_public_key_str = json.loads(peer_conec.recv(4096).decode())["public_key"]
+        peer_public_key = deserialize_public_key(peer_public_key_str)
+        
         while True:
-            data = peer_conec.recv(4096).decode()
-
+            encrypted_data = peer_conec.recv(4096).decode()
+            data = decrypt_with_private_key(self.private_key, encrypted_data)
             peer_requisition = json.loads(data)
+            
             cmd = peer_requisition.get("cmd")
             
             match cmd:
                 case "login":
                     response = self.process_peer_login(peer_requisition)
-                    peer_conec.send(json.dumps(response).encode())
+                    encrypted_response = encrypt_with_public_key(peer_public_key, json.dumps(response))
+                    peer_conec.send(encrypted_response.encode())
 
                     if response.get("status") == "ok":
                         self.process_chat()
                 case "register":
                     response = self.process_peer_register(peer_requisition)
-                    peer_conec.send(json.dumps(response).encode())
+                    encrypted_response = encrypt_with_public_key(peer_public_key, json.dumps(response))
+                    peer_conec.send(encrypted_response.encode())
 
                     if response.get("status") == "ok":
                         continue
@@ -56,8 +68,8 @@ class Tracker:
         user = peer_req.get("usr")
         password = peer_req.get("password")
 
-        if user in self.users and self.users[user]["password"] == password:
-            self.users[user]["active"] = True;
+        if user in self.users and self.users[user]["password"] == hash_password(password):
+            self.users[user]["active"] = True
             return {"status": "ok", "message": "Login bem sucedido"}
 
         return {"status": "error", "message": "Usuario ou senha incorretos"}
@@ -71,7 +83,7 @@ class Tracker:
         else:
             new_user = {
                 user: {
-                    "password": password,
+                    "password": hash_password(password),
                     "active": False
                 }
             }
@@ -81,11 +93,7 @@ class Tracker:
 
     def process_chat(self):
         ...
-    
-
-        
 
 if __name__ == "__main__":
     tracker = Tracker()
     tracker.listen()
-        
