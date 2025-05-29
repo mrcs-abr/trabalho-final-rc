9ef1a684f3758a48ee84017791dad96d3fa7a700
Marcos Andre <marcos.br258@gmail.com>
2025-05-29 15:02:34 -0300

    Added tracker.py

---
+++ trabalho-rc/peer.py
@@ -0,0 +1,124 @@
+import socket, json, hashlib
+from encrypt_utils import hash_password
+
+class Peer:
+    def __init__(self, tracker_host="localhost", tracker_port = 6000, peer_port = 5500):
+        self.tracker_host = tracker_host
+        self.tracker_port = tracker_port
+        self.tracker_info = (tracker_host, tracker_port)
+        self.peer_port = peer_port
+        self.peer_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        self.peer_socket.connect(self.tracker_info)
+    def start(self):
+
+        while True:
+            print("========== Tela inicial Chatp2p ==========")
+            print("Escolha uma opção: ")
+            print("[1] Login")
+            print("[2] Registrar")
+            print("[3] Sair")
+
+            try:
+                option = int(input("->"))
+            except:
+                print("Opção inválida")
+                option = int(input("->"))
+                continue
+            
+            match option:
+                case 1:
+                    self.process_login()
+                case 2:
+                    self.process_register()
+                case 3:
+                    self.peer_socket.close()
+                    break
+    
+    def process_login(self):
+        print("========== Login ==========")
+        user = input("Digite seu usuario: ").strip()
+        input_password = input("Digite sua senha: ").strip()
+        password = hash_password(input_password)
+        requisition = {
+            "cmd": "login", 
+            "usr": user, 
+            "password": input_password,
+            "port": self.peer_port,
+        }
+
+        self.peer_socket.send(json.dumps(requisition).encode())
+        data = self.peer_socket.recv(1024).decode()
+        response = json.loads(data)
+        print(response)
+
+        if response.get("status") == "ok":
+            print(response.get("message"))
+            self.process_chat_functions()
+
+        else:
+            print(response.get("message"))
+            option = input("Deseja tentar novamente? [s/n]").strip()
+
+            if option == "s":
+                self.process_login()
+            else:
+                self.start()
+
+    def process_register(self):
+        print("========== Registro de usuario ==========")
+        user = input("Digite um nome de usuario: ").strip()
+        input_password = input("Digite uma senha: ").strip()
+
+        if user and input_password:
+            password = hash_password(input_password)
+            requisition = {
+                "cmd": "register", 
+                "usr": user, 
+                "password": password,
+                "port": self.peer_port,
+            }
+
+            self.peer_socket.send(json.dumps(requisition).encode())
+            data = self.peer_socket.recv(1024).decode()
+            response = json.loads(data)
+            print(response)
+        else:
+            print("Usuario ou senha invalidos")
+            self.process_register()
+
+    
+    def process_chat_functions():
+        while True:
+            print("========== Chatp2p ==========")
+            print("Escolha uma opção: ")
+            print("[1] Listar usuarios ativos")
+            print("[2] Listar salas disponiveis")
+            print("[3] Criar sala")
+            print("[4] Iniciar chat privado")
+            
+            try:
+                option = int(input("->"))
+            except:
+                print("Opção inválida")
+                option = int(input("->"))
+                continue
+            
+            match option:
+                case 1:
+                    ...
+                case 2:
+                    ...
+                case 3:
+                    ...
+                case 4:
+                    ...
+
+
+
+
+if __name__ == "__main__":
+    peer = Peer()
+    peer.start()
+
+
---
+++ trabalho-rc/tracker.py
@@ -0,0 +1,94 @@
+import socket, json
+from threading import Thread
+
+class Tracker:
+    def __init__(self, host='0.0.0.0', port=6000, max_connec = 5):
+        self.host = host
+        self.port = port
+        self.server_info = (host, port)
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.bind(self.server_info)
+        self.server.listen(max_connec)
+
+        # Chat storage
+        self.users = {
+            "tonhao": {
+                "password": "senha123",
+                'active': False
+            }
+        }
+        self.active_peers = {}
+        self.chat_rooms = {}
+
+        print(f"Tracker iniciado em {self.host}:{self.port}")
+        print("Aguardando conexões...")
+
+    def listen(self):
+        while True:
+            peer_conec, address = self.server.accept()
+            print("Conexao de: " + str(address))
+            Thread(target=self.process_new_peer, args=(peer_conec, address)).start()
+
+    def process_new_peer(self, peer_conec, address):  
+        while True:
+            data = peer_conec.recv(4096).decode()
+
+            peer_requisition = json.loads(data)
+            cmd = peer_requisition.get("cmd")
+            
+            match cmd:
+                case "login":
+                    response = self.process_peer_login(peer_requisition)
+                    peer_conec.send(json.dumps(response).encode())
+
+                    if response.get("status") == "ok":
+                        self.process_chat()
+                case "register":
+                    response = self.process_peer_register(peer_requisition)
+                    peer_conec.send(json.dumps(response).encode())
+
+                    if response.get("status") == "ok":
+                        continue
+                    else:
+                        self.process_peer_register()
+                        
+
+
+
+    def process_peer_login(self, peer_req):
+        user = peer_req.get("usr")
+        password = peer_req.get("password")
+
+        if user in self.users and self.users[user]["password"] == password:
+            self.users[user]["active"] = True;
+            return {"status": "ok", "message": "Login bem sucedido"}
+
+        return {"status": "error", "message": "Usuario ou senha incorretos"}
+
+    def process_peer_register(self, peer_req):
+        user = peer_req.get("usr")
+        password = peer_req.get("password")
+
+        if user in self.users:
+            return {"status": "error", "message": "Usuario ja existente"}
+        else:
+            new_user = {
+                user: {
+                    "password": password,
+                    "active": False
+                }
+            }
+            self.users.update(new_user)
+            print(self.users)
+            return {"status": "ok", "message": "Usuario registrado!"}
+
+    def process_chat():
+        ...
+    
+
+        
+
+if __name__ == "__main__":
+    tracker = Tracker()
+    tracker.listen()
+        
